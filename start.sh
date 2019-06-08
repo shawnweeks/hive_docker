d0adb4624c24d4a02c3e38aa2dce3df06fe6a8db
Shawn Weeks <sweeks@weeksconsulting.us>
2019-06-07 19:17:31 -0500

    Fixed TEZ Split Issue and updated Memory to 4gb

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export HADOOP_HEAPSIZE=1024
+export HADOOP_HEAPSIZE=4096
 
 /usr/hdp/current/hive-server2/bin/hiveserver2 > /tmp/hive/hive.out 2> /tmp/hive/hive.err &
 tail -F /tmp/hive/hive.log /tmp/hive/hive.out /tmp/hive/hive.err

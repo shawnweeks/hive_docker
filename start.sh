fe3cabce5ceb6ad8f5dfee7a1197433e9fcda681
Shawn Weeks <sweeks@weeksconsulting.us>
2019-06-10 12:47:55 -0500

    Starting Metastore Seperately

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 export HADOOP_HEAPSIZE=4096
 
-/usr/hdp/current/hive-server2/bin/hiveserver2 --hiveconf hive.root.logger=INFO,console
+/usr/hdp/current/hive-server2/bin/hive --service metastore --hiveconf hive.root.logger=INFO,console &
+/usr/hdp/current/hive-server2/bin/hive --service hiveserver2 --hiveconf hive.root.logger=INFO,console
